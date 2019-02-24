4c83607b5572d69f3e3a1ccddc7041ca1afaceea
Fifi Art <44811907+fifiinart@users.noreply.github.com>
2019-02-23 21:54:22 -0500

    Update Enemy.js

--- base/Enemy.js
+++ base/Enemy.js
@@ -3,13 +3,14 @@ const general = {
   range: require('./general/range.js')
 };
 module.exports = class Enemy {
-  constructor(name, description, level, health = 20, damageRange = general.range(5, 10)) {
+  constructor(name, description, level, health = 20, damageRange = general.range(5, 10), rewards) {
     this.name = name;
     this.description = description;
     this.level = level;
     this.health = health;
     this.allHealth = health;
     this.damageRange = damageRange;
+    this.rewards = rewards;
     //-----
   }
   toString() {
