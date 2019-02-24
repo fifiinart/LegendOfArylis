91e9f1beb8c9f7b5a96b5238543305bf79e7ac7d
Fifi Art <44811907+fifiinart@users.noreply.github.com>
2019-02-23 21:59:38 -0500

    Add rewards

--- enemies/Slime.js
+++ enemies/Slime.js
@@ -2,6 +2,9 @@ const Enemy = require('./../base/Enemy.js');
 const general = require('./../general/general.js');
 module.exports = class Slime extends Enemy {
   constructor(level) {
-    super('Slime', 'A jelly-like creature you don\'t want to touch.', level, level * 10, general.range(level * 5, level * 10));
+    super('Slime', 'A jelly-like creature you don\'t want to touch.', level, level * 10, general.range(level * 5, level * 10), {
+      gold: general.random(general.range(level * 5, level * 5 - 5)),
+      xp: general.random(general.range(level * 5, level * 5 - 5))
+    });
   }
 }
