15864aeb4623a4565773bbd6764712a7a8fc661c
Fifi Art <44811907+fifiinart@users.noreply.github.com>
2019-02-23 19:36:48 -0500

    Create Item.js

---
+++ base/Item.js
@@ -0,0 +1,12 @@
+class Item {
+  constructor(name, description, tier, itemClass) {
+    this.name = name;
+    this.itemClass = itemClass;
+    this.tier = tier;
+    this.name = name;
+    this.description = description;
+  }
+  toString () {
+    
+  }
+}
